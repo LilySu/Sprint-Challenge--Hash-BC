ef266a9a5c05eb7f6110ba1815ef1fa3ecc31571
LilySu <LilySu@users.noreply.github.com>
2020-05-08 16:23:29 -0400

    done with ex1 - ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -10,19 +10,62 @@ def intersection(arrays):
 
     hash_dict = {}
     result = []
-    for first in arrays[0]:
-        hash_dict[first] = 1
-    for i in arrays[1:]:
-        for after in i:
-            if after in hash_dict:
-                hash_dict[after] += 1
+    for i in arrays[0]:# for each item in the first list of array
+        hash_dict[i] = 1 # insert into dictionary set key to 1
+    for listitem in arrays[1:]:# for each item thereafter
+        for i in listitem:
+            if i in hash_dict:
+                hash_dict[i] += 1
 
     result = []
-    for i in hash_dict:
-        if hash_dict[i] == len(arrays):
-            result.append(i)
+    for i in hash_dict:# for each item in dictionary
+        if hash_dict[i] == len(arrays): # if value is equal to the length of the array
+            result.append(i) # append to result
     return result
 
+
+
+
+    # for i in arrays:
+    #     length = len(arrays)
+    #     hashtable = HashTable(length)
+    #     new = [None] * length
+
+    # # Initialize an empty hashtable
+    # # Iterate through the first array and put every element of the first array in the hashtable
+    # for i in range(len(arrays)):
+    #     hash_table_insert(hashtable, i, arrays[i])
+
+    # for j in arrays:
+    #     new = hash_table_retrieve(hashtable, i)
+
+    # for i in new:
+    #     # if i in arrays:
+    #     print([list(filter(lambda x: i in new, sublist)) for sublist in arrays])
+            
+    
+    # for j in range(len(arrays)):
+    #     value = hash_table_retrieve(hashtable, i)
+    #     if value in 
+    #     print(hash_table_retrieve(hashtable, i))
+    # for i in new:
+    #     print(new.values)
+    
+    # for j in range(len(arrays)):
+    #     if 
+    #     repeat = hash_table_retrieve(hashtable, i)
+    #     if repeat is not None:
+    #         return repeat
+    # return None
+
+
+    # # # For every element x of the next array
+    # for j in range(len(arrays)):    
+    #     # Search x in the set hashtable. If x is present, then print it.
+    # #     new[j] = hash_table_retrieve(hashtable, new[j])
+    # # return new
+    #     print(new[j])
+    # print(new)
 
 
 if __name__ == "__main__":
